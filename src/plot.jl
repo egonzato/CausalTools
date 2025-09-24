341296408e6510af70d88a7feb33f2066fc417d9
egonzato <e.gonzato@amsterdamumc.nl>
2025-09-24 11:01:14 +0200

    Added dataset to use in README and filling of density

--- src/plot.jl
+++ src/plot.jl
@@ -4,14 +4,18 @@ using Plots, StatsPlots
 using ..IPTW: IPTWResult
 
 function Plots.plot(result::IPTWResult)
-    df = result.dataset
-    treat_col = result.treatment
+    df=result.dataset
+    treat_col=result.treatment
     @df df density(:weight,
-                     group=df[!,treat_col],
-                     legend=:topright,
-                     xlabel="Weight",
-                     ylabel="Density",
-                     title="Distribution of weights by treatment")
+                   seriestype = :density,
+                   group=df[!,treat_col],
+                   legend=:topright,
+                   xlabel="Weight",
+                   ylabel="Density",
+                   title="Distribution of weights by treatment",
+                   lw = 2,
+                   fillrange = 0,
+                   fillalpha = 0.35,)
 end
 
 end
